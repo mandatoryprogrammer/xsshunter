3334a2536fd9a7a363368d697c48e7ec1e88c3e0
Luke Young <luke@hydrantlabs.org>
2016-05-30 20:08:13 -0700

    Use YAML safe_load instead of load just in case

--- gui/guiserver.py
+++ gui/guiserver.py
@@ -7,7 +7,7 @@ import yaml
 
 try:
     with open( '../config.yaml', 'r' ) as f:
-        settings = yaml.load( f )
+        settings = yaml.safe_load( f )
 except IOError:
     print "Error reading config.yaml, have you created one? (Hint: Try running ./generate_config.py)"
     exit()
